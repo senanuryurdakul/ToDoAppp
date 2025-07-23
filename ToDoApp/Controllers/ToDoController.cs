e86f91328423bd20a2e6a773ee5bb59cfe5373aa
Senanur Yurdakul <Senanuryurdakul0@gmail.com>
2025-07-23 21:18:37 +0300

    Add project files.

---
+++ ToDoApp/Controllers/ToDoController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoApp.Models;
+using System.Linq;
+
+namespace ToDoApp.Controllers
+{
+    public class ToDoController : Controller
+    {
+        private static List<ToDoItem> ToDos = new List<ToDoItem>();      //Görevleri Listele
+        public IActionResult Index()
+        {
+
+            return View(ToDos);
+        }
+        [HttpPost]
+        public IActionResult Add(string Tittle)
+        {
+            if (!string.IsNullOrWhiteSpace(Tittle))
+            {
+                ToDos.Add(new ToDoItem
+                {
+                    Id = ToDos.Count + 1,
+                    Title = Tittle
+                });
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var item =ToDos.FirstOrDefault(t=> t.Id ==id);
+            if (item != null)
+            {
+                ToDos.Remove(item);
+
+                return
+                    RedirectToAction("Index");
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ ToDoApp/Models/ToDoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace ToDoApp.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
